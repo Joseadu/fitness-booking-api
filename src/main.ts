a57287889d745908ff6535cde949cdfed267aa42
Jose Diaz <jose.uk94@gmail.com>
2026-01-08 00:58:15 +0100

    chore: enable CORS for development (localhost:4200)

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:4200', // Frontend Angular
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  app.enableCors({
+    origin: 'http://localhost:4200', // Frontend Angular
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
