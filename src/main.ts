fbafd5c61d19273628dd978efba45ab68e1720af
Jose Diaz <jose.uk94@gmail.com>
2026-01-11 00:53:57 +0100

    chore: add fly.io deployment config and fix CORS

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4200', // Frontend Angular
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
-  app.enableCors({
-    origin: 'http://localhost:4200', // Frontend Angular
+    origin: [
+      'http://localhost:4200',
+      'http://fitness-booking.joseadu.com',
+      'https://fitness-booking.joseadu.com'
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
